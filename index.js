481a933026585503c93a6746a5b75a75bda0d2c4
DorkoSpron0 <tareassec9@gmail.com>
2022-01-30 20:10:53 -0500

    Creamos archivos necesarios y ya

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require('express');
 const app = express()
 
 app.set('port', process.env.PORT || 8080);
+app.set('view engine','ejs')
+app.set('views', './views')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', require('./routes/routes'))
 
 app.listen(app.get('port'), () => {
-	console.log('Server Started');
+	console.log(`Server Started ${app.get('port')}`);
 })
