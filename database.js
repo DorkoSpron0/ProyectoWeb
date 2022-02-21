daaed43e7ee2afc287bcb1edf17787a524dbee6e
DorkoSpron0 <tareassec9@gmail.com>
2022-02-20 19:09:31 -0500

    Base de datos online

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const Mongoose = require("mongoose");
 
-Mongoose.connect('mongodb://localhost/ProyectoWeb')
+Mongoose.connect('mongodb+srv://dorkosprono:rRbgESQlT9ahei6r@cluster0.qsqw4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
     .then(db => console.log('DB'))
     .catch(err => console.error(err))
