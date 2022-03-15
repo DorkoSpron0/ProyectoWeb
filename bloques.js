263c95d9666211321a572b7a7a4e413ec57df163
Tazana2 <danisanmez@gmail.com>
2022-03-15 18:10:13 -0500

    Se quitó oficialmente Bootstrap de todo

--- bloques.js
+++ bloques.js
@@ -6,7 +6,7 @@ module.exports = [
             id: 001,
             title: 'Bloque 01',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia voluptates, perferendis veritatis eaque facilis ullam perspiciatis quas, optio molestiae molestias placeat similique? Itaque doloribus tempora aperiam eius modi. Ullam ipsa, asperiores sequi voluptate sit modi cumque voluptatibus assumenda, temporibus consequuntur non pariatur, nam praesentium voluptas fugit. Eum, nam eveniet!',
-            img: 'https://i.ytimg.com/vi/Oy7fS5eoXAw/maxresdefault.jpg'
+            img: 'https://www.vandelaydesign.com/wp-content/uploads/adidas-1.jpg'
         }
     },
     {
@@ -16,7 +16,7 @@ module.exports = [
             id: 002,
             title: 'Bloque 02',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia voluptates, perferendis veritatis eaque facilis ullam perspiciatis quas, optio molestiae molestias placeat similique? Itaque doloribus tempora aperiam eius modi. Ullam ipsa, asperiores sequi voluptate sit modi cumque voluptatibus assumenda, temporibus consequuntur non pariatur, nam praesentium voluptas fugit. Eum, nam eveniet!',
-            img: 'https://i.ytimg.com/vi/Oy7fS5eoXAw/maxresdefault.jpg'
+            img: 'https://www.vandelaydesign.com/wp-content/uploads/adidas-1.jpg'
         }
     },
     {
@@ -26,7 +26,7 @@ module.exports = [
             id: 003,
             title: 'Bloque 03',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia voluptates, perferendis veritatis eaque facilis ullam perspiciatis quas, optio molestiae molestias placeat similique? Itaque doloribus tempora aperiam eius modi. Ullam ipsa, asperiores sequi voluptate sit modi cumque voluptatibus assumenda, temporibus consequuntur non pariatur, nam praesentium voluptas fugit. Eum, nam eveniet!',
-            img: 'https://i.ytimg.com/vi/Oy7fS5eoXAw/maxresdefault.jpg'
+            img: 'https://www.vandelaydesign.com/wp-content/uploads/adidas-1.jpg'
         }
     },
     {
@@ -36,7 +36,7 @@ module.exports = [
             id: 004,
             title: 'Bloque 04',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia voluptates, perferendis veritatis eaque facilis ullam perspiciatis quas, optio molestiae molestias placeat similique? Itaque doloribus tempora aperiam eius modi. Ullam ipsa, asperiores sequi voluptate sit modi cumque voluptatibus assumenda, temporibus consequuntur non pariatur, nam praesentium voluptas fugit. Eum, nam eveniet!',
-            img: 'https://i.ytimg.com/vi/Oy7fS5eoXAw/maxresdefault.jpg'
+            img: 'https://www.vandelaydesign.com/wp-content/uploads/adidas-1.jpg'
         }
     },
     {
@@ -46,7 +46,7 @@ module.exports = [
             id: 005,
             title: 'Bloque 05',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia voluptates, perferendis veritatis eaque facilis ullam perspiciatis quas, optio molestiae molestias placeat similique? Itaque doloribus tempora aperiam eius modi. Ullam ipsa, asperiores sequi voluptate sit modi cumque voluptatibus assumenda, temporibus consequuntur non pariatur, nam praesentium voluptas fugit. Eum, nam eveniet!',
-            img: 'https://i.ytimg.com/vi/Oy7fS5eoXAw/maxresdefault.jpg'
+            img: 'https://www.vandelaydesign.com/wp-content/uploads/adidas-1.jpg'
         }
     },
     {
@@ -56,7 +56,7 @@ module.exports = [
             id: 006,
             title: 'Bloque 06',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia voluptates, perferendis veritatis eaque facilis ullam perspiciatis quas, optio molestiae molestias placeat similique? Itaque doloribus tempora aperiam eius modi. Ullam ipsa, asperiores sequi voluptate sit modi cumque voluptatibus assumenda, temporibus consequuntur non pariatur, nam praesentium voluptas fugit. Eum, nam eveniet!',
-            img: 'https://i.ytimg.com/vi/Oy7fS5eoXAw/maxresdefault.jpg'
+            img: 'https://www.vandelaydesign.com/wp-content/uploads/adidas-1.jpg'
         }
     },
     {
@@ -66,7 +66,7 @@ module.exports = [
             id: 007,
             title: 'Bloque 07',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia voluptates, perferendis veritatis eaque facilis ullam perspiciatis quas, optio molestiae molestias placeat similique? Itaque doloribus tempora aperiam eius modi. Ullam ipsa, asperiores sequi voluptate sit modi cumque voluptatibus assumenda, temporibus consequuntur non pariatur, nam praesentium voluptas fugit. Eum, nam eveniet!',
-            img: 'https://i.ytimg.com/vi/Oy7fS5eoXAw/maxresdefault.jpg'
+            img: 'https://www.vandelaydesign.com/wp-content/uploads/adidas-1.jpg'
         }
     },
 ]
