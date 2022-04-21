12a882c08197fa758aa0e144f3dfe7d7ab0a3ed1
dorkospron0 <tareassec9@gmail.com>
2022-04-21 17:21:00 -0500

    Variables de entorno con dotenv

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
-	password: 'pendejo4172286',
-	database: 'inemapstest01',
-	port: '5432'
+	user: process.env.POSTGRES_USR,
+	host: process.env.POSTGRES_HOST,
+	password: process.env.POSTGRES_PSSW,
+	database: process.env.POSTGRES_DATABASE,
+	port: process.env.POSTGRES_PORT
 })
 
 module.exports = pool
